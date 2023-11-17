48f5bd00d61efe12f8a44738c02fa87e0cadaddc
rudrodip <official.rudrodipsarker@gmail.com>
2023-11-17 13:54:27 +0600

    initial setup

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const cat1Dom = document.querySelector('#cat1')
+const cat2Dom = document.querySelector('#cat2')
+
+cat1Dom.addEventListener('click', () => {
+  console.log('cat1')
+})
+
+cat2Dom.addEventListener('click', () => {
+  console.log('cat2')
+})
+
+let cat1 = ''
+let cat2 = ''
